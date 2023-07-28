9bb947ad90d0039e04975499b774338787b95cbb
sarabk91 <133173285+sarabk91@users.noreply.github.com>
2023-07-27 18:25:18 -0700

    pushing my hmw

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,14 @@
 # Import the dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 
@@ -8,19 +18,23 @@
 
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(engine, reflect=True)
 
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
+app = Flask(__name__)
 
 
 
@@ -28,3 +42,14 @@
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def home():
+    return (
+        f"Welcome to the Climate App API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
